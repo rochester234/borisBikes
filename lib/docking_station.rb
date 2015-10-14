dfd3eb7c943f9e5c93a336598861d284e40adf4b
“DovileSand” <dovile.sand@protonmail.ch>
2015-10-14 11:52:15 +0100

    Docking method created

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,7 +2,13 @@ require_relative 'bike'
 
 class DockingStation
 
+attr_reader :bike
+
 def release_bike
+  @bike = Bike.new
+end
+
+def dock(bike)
 end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,17 @@ require 'docking_station'
 
 describe DockingStation do
   it {is_expected.to(respond_to(:release_bike))}
+
+  it 'expects to be able to dock a bike at the docking station' do
+    expect(subject).to respond_to(:dock).with(1).argument
+  end
+
+  it 'expects to see a bike that has been docked' do
+    expect(subject).to respond_to(:bike)
+  end
+
+  it 'is an instance of the Bike class' do
+    expect(subject.release_bike).to be_instance_of Bike
+  end
+
 end
