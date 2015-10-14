038b7aac7b803785321db3ff50b002cb2cc060b2
“DovileSand” <dovile.sand@protonmail.ch>
2015-10-14 10:46:09 +0100

    Second day initial ammendments

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,23 +1,8 @@
+require_relative 'bike'
+
 class DockingStation
 
-attr_accessor
-
-def initialise
-end
-
 def release_bike
-end
-
-end
-
-class Bike
-
-attr_accessor
-
-def initialise
-end
-
-def working?
 end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,3 @@
-
 require 'docking_station'
 
 describe DockingStation do
