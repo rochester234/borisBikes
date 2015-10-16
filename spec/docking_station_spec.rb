7299ab9b105771ace9248409d383217664ce5382
Mateja <mateja@Matejas-MacBook-Pro.local>
2015-10-16 13:53:40 +0100

    adding stubs

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,7 @@
 require 'docking_station'
 
 describe DockingStation do
+  let (:bike) { double :bike}
   it {is_expected.to(respond_to(:release_bike))}
 
   it 'expects to be able to dock a bike at the docking station' do
@@ -25,6 +26,7 @@ describe DockingStation do
     describe '#release_bike' do
       it 'releases a bike' do
         bike = double(:bike)
+        allow(bike).to receive(:working).and_return(true)
         subject.dock(bike)
         expect(subject.release_bike).to eq bike
       end
@@ -53,7 +55,8 @@ describe DockingStation do
 
     it 'a bike is docked with an working? attribute of false' do
       my_bike = double(:bike)
-      bike_status = my_bike.working?(false)
+      allow(my_bike).to receive(:working).and_return(false)
+    #  bike_status = my_bike.working?(false)
       docking_status = subject.dock(my_bike)
       expect(docking_status.working).to eq false
 
@@ -61,7 +64,8 @@ describe DockingStation do
 
     it 'does not release broken bikes' do
       my_bike = double(:bike)
-      my_bike.working?(false)
+      allow(my_bike).to receive(:working).and_return(false)
+    #  my_bike.working?(false)
       subject.dock(my_bike)
       expect {subject.release_bike}.to raise_error 'The bike is broken'
     end
@@ -70,8 +74,9 @@ describe DockingStation do
       my_bike = double(:bike)
       my_bike2 = double(:bike)
       my_bike3 = double(:bike)
-
-      my_bike3.working?(false)
+      allow(my_bike3).to receive(:working).and_return(false)
+      allow(my_bike).to receive(:working).and_return(true)
+      allow(my_bike2).to receive(:working).and_return(true)
 
       subject.dock(my_bike3)
       subject.dock(my_bike2)
